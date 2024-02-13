019472c9500f0d10622c8a690bdb74c045a252bc
leandro valora <89744628+Leandro-Valora@users.noreply.github.com>
2024-02-13 12:11:49 +0100

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -102,7 +102,7 @@ function Login() {
         if (validationPerformed) {
             const handleValidation = () => {
                 if (errors.email === "" && errors.password === "") {
-                    axios.post("http://localhost:8081/login", values).then(res => {
+                    axios.post("https://esempio-sito.onrender.com/login", values).then(res => {
                         if (res.data === "Success") {
                             navigate("/home");
                         } else {
