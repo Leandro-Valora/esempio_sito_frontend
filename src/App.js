84a323cd3d86a0584213289d9dc13b89860a74dc
leandro valora <89744628+Leandro-Valora@users.noreply.github.com>
2024-02-13 12:04:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Login from './Login';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Signup from './Signup';
 import Home from './Home';
 
